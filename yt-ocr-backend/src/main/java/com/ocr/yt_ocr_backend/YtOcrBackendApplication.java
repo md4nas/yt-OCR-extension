d40f84a55bd94ca382069d325afa4a489b03d596
md4nas <md.anas1028@gmail.com>
2025-08-23 09:09:47 +0530

    first commit

---
+++ yt-ocr-backend/src/main/java/com/ocr/yt_ocr_backend/YtOcrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ocr.yt_ocr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtOcrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtOcrBackendApplication.class, args);
+	}
+
+}
---
+++ yt-ocr-backend/src/test/java/com/ocr/yt_ocr_backend/YtOcrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocr.yt_ocr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtOcrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
