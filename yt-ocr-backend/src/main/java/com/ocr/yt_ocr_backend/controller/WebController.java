c9a5bc1586bf48767a3f5cbc3f3cbcc91161b828
md4nas <md.anas1028@gmail.com>
2025-08-30 19:36:35 +0530

    created he webController for proper redirection

---
+++ yt-ocr-backend/src/main/java/com/ocr/yt_ocr_backend/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.ocr.yt_ocr_backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
